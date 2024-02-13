b9d26d4ee1997356715fcdc2fba974cce907e2a1
saqlaan <saqlaanawan@gmail.com>
2024-02-13 02:36:21 +0100

    feat: firebase authentication

---
+++ src/screens/Login/Login.tsx
@@ -0,0 +1,114 @@
+import React, { useState } from "react";
+import {
+  ActivityIndicator,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+import { login } from "@/services/firebase/auth";
+import firebaseErrors from "@/services/firebase/firebaseErrors";
+import type { ApplicationScreenProps } from "@/types/navigation";
+
+function Login({ navigation }: ApplicationScreenProps) {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const handleLogin = () => {
+    setIsLoading(true);
+    login({ email, password })
+      .then()
+      .catch((error) => {
+        if (error.code) {
+          setError(firebaseErrors[error.code]);
+        }
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.logo}>Login</Text>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Email..."
+          placeholderTextColor="#003f5c"
+          onChangeText={(text) => setEmail(text)}
+          autoCapitalize="none"
+          inputMode="email"
+        />
+      </View>
+      <View style={styles.inputView}>
+        <TextInput
+          secureTextEntry
+          style={styles.inputText}
+          placeholder="Password..."
+          placeholderTextColor="#003f5c"
+          onChangeText={(text) => setPassword(text)}
+        />
+      </View>
+      {error && <Text style={styles.error}>{error}</Text>}
+      <TouchableOpacity style={styles.loginBtn} onPress={handleLogin}>
+        {isLoading ? (
+          <ActivityIndicator />
+        ) : (
+          <Text style={styles.loginText}>LOGIN</Text>
+        )}
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    fontWeight: "bold",
+    fontSize: 50,
+    color: "#fb5b5a",
+    marginBottom: 40,
+  },
+  inputView: {
+    width: "80%",
+    backgroundColor: "#ccc",
+    borderRadius: 25,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: "center",
+    padding: 20,
+  },
+  inputText: {
+    height: 50,
+    color: "#003f5c",
+  },
+  loginBtn: {
+    width: "80%",
+    backgroundColor: "#fb5b5a",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    marginBottom: 10,
+  },
+  loginText: {
+    color: "white",
+  },
+  error: {
+    fontSize: 14,
+    color: "black",
+  },
+});
+
+export default Login;
