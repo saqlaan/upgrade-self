ce5154389fb553a03f2f3702c0add8588b39acd4
saqlaan <saqlaanawan@gmail.com>
2024-02-16 02:51:02 +0100

    Android build fixed

--- ios/upgradeLabsTests/upgradeLabsTests.m
+++ ios/upgradeLabsTests/upgradeLabsTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface upgradeLabsTests : XCTestCase
+@interface upgradelabsTests : XCTestCase
 
 @end
 
-@implementation upgradeLabsTests
+@implementation upgradelabsTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
