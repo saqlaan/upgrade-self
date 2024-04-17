6fe9b751260031f33d03d512e19d96f87fef3919
saqlaan <saqlaanawan@gmail.com>
2024-04-17 16:43:07 +0200

    create appointment api added

--- firebase/functions/src/app/controllers/servicesController.ts
+++ firebase/functions/src/app/controllers/servicesController.ts
@@ -32,20 +32,18 @@ export const getCenterServices = async (
   return;
 };
 
-export const createServiceBooking = async (
-  req: Request<unknown, unknown, unknown, GetCentersQueryParams>,
-  res: Response,
-) => {
+export const createAppointment = async (req: Request, res: Response) => {
   try {
-    const { centerId, countryCode, size, page } = req.query;
-    if (!centerId || !countryCode) {
-      res.status(403).json({ message: "Center id or country code is missing" });
+    const { guestId, centerId, serviceId, date, countryCode } = req.body;
+    if (!centerId || !serviceId || !guestId || !date) {
+      res.status(403).json({ message: "Center id or country code or serviceId or date is missing" });
     }
-    const data = await serviceBooking.getCenterServices({
+    const data = await serviceBooking.createAppointment({
       centerId,
       organization: countryCode,
-      size,
-      page,
+      date,
+      guestId,
+      serviceId,
     });
     return res.json(data);
   } catch (error) {
@@ -53,4 +51,53 @@ export const createServiceBooking = async (
     res.status(403).json({ message: "Error getting the services" });
   }
   return;
+};
+
+export const getSlots = async (req: Request, res: Response) => {
+  try {
+    const bookingId = req.params.bookingId;
+    const { countryCode } = req.body;
+    const data = await serviceBooking.getSlots({
+      bookingId,
+      organization: countryCode,
+    });
+    return res.json(data);
+  } catch (error) {
+    console.error("Error getting the services", error);
+    res.status(403).json({ message: "Error getting the services" });
+    return null;
+  }
+};
+
+export const reserveSlot = async (req: Request, res: Response) => {
+  try {
+    const bookingId = req.params.bookingId;
+    const { countryCode, slotTime } = req.body;
+    const data = await serviceBooking.reserveSlot({
+      bookingId,
+      organization: countryCode,
+      time: slotTime,
+    });
+    return res.json(data);
+  } catch (error) {
+    console.error("Error reserving the slot", error);
+    res.status(403).json({ message: "Error reserving the slot" });
+    return null;
+  }
+};
+
+export const confirmBooking = async (req: Request, res: Response) => {
+  try {
+    const bookingId = req.params.bookingId;
+    const { countryCode } = req.body;
+    const data = await serviceBooking.confirmBooking({
+      bookingId,
+      organization: countryCode,
+    });
+    return res.json(data);
+  } catch (error) {
+    console.error("Error confirming the slot", error);
+    res.status(403).json({ message: "Error confirming the slot" });
+    return null;
+  }
 };
--- firebase/functions/src/services/zenoti/serviceBooking.ts
+++ firebase/functions/src/services/zenoti/serviceBooking.ts
@@ -23,3 +23,108 @@ export const getCenterServices = async ({
     throw new Error("Failed to retrieve center services");
   }
 };
+
+export const createAppointment = async ({
+  organization,
+  centerId,
+  serviceId,
+  guestId,
+  date,
+}: {
+  organization: Organization;
+  centerId: string;
+  serviceId: string;
+  guestId: string;
+  date: string; // Assuming date format is a string
+}) => {
+  try {
+    const response = await axios.post(
+      `/bookings?is_double_booking_enabled=false`,
+      {
+        is_only_catalog_employees: true,
+        center_id: centerId,
+        date,
+        guests: [
+          {
+            id: guestId,
+            items: [
+              {
+                item: {
+                  id: serviceId,
+                },
+              },
+            ],
+          },
+        ],
+      },
+      {
+        headers: requestHeaders[organization],
+      },
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to create appointment");
+  }
+};
+
+export const getSlots = async ({ bookingId, organization }: { bookingId: string; organization: Organization }) => {
+  try {
+    const response = await axios.get(`bookings/${bookingId}/slots`, {
+      headers: requestHeaders[organization],
+    });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to create appointment");
+  }
+};
+
+export const reserveSlot = async ({
+  bookingId,
+  time,
+  organization,
+}: {
+  bookingId: string;
+  time: string;
+  organization: Organization;
+}) => {
+  try {
+    const response = await axios.post(
+      `bookings/${bookingId}/slots/reserve`,
+      {
+        slot_time: time,
+      },
+      {
+        headers: requestHeaders[organization],
+      },
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to create appointment");
+  }
+};
+
+export const confirmBooking = async ({
+  bookingId,
+  organization,
+}: {
+  bookingId: string;
+  organization: Organization;
+}) => {
+  try {
+    console.log({ bookingId, organization });
+    const response = await axios.post(
+      `bookings/${bookingId}/slots/confirm`,
+      {},
+      {
+        headers: requestHeaders[organization],
+      },
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to create appointment");
+  }
+};
