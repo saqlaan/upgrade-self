fbbdc33eb9765e554ecccf8aad011c4ab89328e8
saqlaan <saqlaanawan@gmail.com>
2024-05-13 16:21:39 -0400

    Set homescreen greeting based on time of day

--- frontend/src/screens/PrivateScreens/HomeScreen/components/HomeHeader.tsx
+++ frontend/src/screens/PrivateScreens/HomeScreen/components/HomeHeader.tsx
@@ -6,12 +6,24 @@ import { BellBingIcon } from "@/theme/assets/icons";
 import { colors } from "@/theme";
 
 const HomeHeader = () => {
+  // get local time
+  const date = new Date();
+  const hours = date.getHours();
+  // set greeting based on time
+  let greeting = "";
+  if (hours < 12) {
+    greeting = "Good Morning!";
+  } else if (hours < 18) {
+    greeting = "Good Afternoon!";
+  } else {
+    greeting = "Good Evening!";
+  }
   return (
     <>
       <Box row justifyContent="space-between" px="5">
         <Box>
           <Text variant="text-xl-bold" mb="1">
-            Good Morning!
+            {greeting}
           </Text>
           <CenterSelector />
         </Box>
