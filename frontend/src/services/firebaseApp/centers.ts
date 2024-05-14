2e9160b1eab4f75867fb9d711652e7a26bc2d492
saqlaan <saqlaanawan@gmail.com>
2024-05-14 00:20:53 -0400

    Use console.log

--- frontend/src/services/firebaseApp/centers.ts
+++ frontend/src/services/firebaseApp/centers.ts
@@ -11,17 +11,17 @@ export const fetchAllCentersData = async (): Promise<CenterType[] | null> => {
     if (error.response) {
       // The request was made and the server responded with a status code
       // that falls out of the range of 2xx
-      console.error(error.response.data);
-      console.error(error.response.status);
-      console.error(error.response.headers);
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
     } else if (error.request) {
       // The request was made but no response was received
       // `error.request` is an instance of XMLHttpRequest in the browser
       // and an instance of http.ClientRequest in node.js
-      console.error(error.request);
+      console.log(error.request);
     } else {
       // Something happened in setting up the request that triggered an Error
-      console.error(error);
+      console.log(error);
     }
     return null;
   }
