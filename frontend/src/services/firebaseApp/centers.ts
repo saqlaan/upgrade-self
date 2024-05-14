6ffa47335511569442641f6ea8be32de54f23516
saqlaan <saqlaanawan@gmail.com>
2024-05-14 00:00:01 -0400

    Improve axios error logging for centers

--- frontend/src/services/firebaseApp/centers.ts
+++ frontend/src/services/firebaseApp/centers.ts
@@ -7,7 +7,21 @@ export const fetchAllCentersData = async (): Promise<CenterType[] | null> => {
     const result = axios.get(Routes.centers);
     return (await result).data;
   } catch (error) {
-    console.log({ error });
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.error(error.response.data);
+      console.error(error.response.status);
+      console.error(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser
+      // and an instance of http.ClientRequest in node.js
+      console.error(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.error('Error', error.message);
+    }
     return null;
   }
 };
